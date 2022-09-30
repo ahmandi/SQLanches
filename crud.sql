357b7a97a6898d0dbd7c16760876d80b71cd1c3b
Amanda <amandard94@gmail.com>
2022-09-30 15:24:33 -0300

    feat: all entries were updated with the required queries

--- crud.sql
+++ crud.sql
@@ -4,31 +4,50 @@
 -- Criação
 
 -- 1)
-
-
+INSERT INTO
+	clientes(nome, lealdade)
+VALUES
+	('Georgia', 0);
 
 -- 2)
-
-
+INSERT INTO
+	pedidos(status, cliente_id)
+VALUES
+	('Recebido', (SELECT clientes.id FROM clientes WHERE clientes.nome = 'Georgia'));
 
 -- 3)
-
-
+INSERT INTO
+	produtos_pedidos(pedido_id, produto_id)
+VALUES
+	(6, 1),
+	(6, 2),
+	(6, 6),
+	(6, 8),
+	(6, 8);
 
 -- Leitura
 
 -- 1)
-
-
+SELECT clientes.*,
+	   pedidos.*,
+	   produtos.*
+FROM produtos_pedidos
+JOIN produtos ON produtos_pedidos.produto_id = produtos.id
+JOIN pedidos  ON produtos_pedidos.pedido_id  = pedidos.id
+JOIN clientes ON produtos_pedidos.pedido_id  = clientes.id
+WHERE clientes.id = 6;
 
 -- Atualização
 
 -- 1)
-
-
+UPDATE
+	clientes
+SET lealdade = 46
+WHERE clientes.id = 6;
 
 -- Deleção
 
 -- 1)
+DELETE FROM clientes WHERE clientes.id = 4;
 
 
--- selects.sql
+++ selects.sql
@@ -1,12 +1,39 @@
 -- Aqui você deve colocar os códigos SQL referentes às
 -- Seleções de dados
 
--- 1)
+-- 1) 
+SELECT pedidos.id,
+	   pedidos.status,
+	   pedidos.cliente_id,
+	   produtos.*
+FROM produtos_pedidos
+JOIN produtos
+ON produtos_pedidos.produto_id = produtos.id
+JOIN pedidos
+ON produtos_pedidos.pedido_id = pedidos.id
+ORDER BY pedidos.id;
 
 -- 2)
+SELECT produtos_pedidos.pedido_id 
+FROM produtos_pedidos
+WHERE produtos_pedidos.produto_id = 6;
 
 -- 3)
+SELECT clientes.nome AS gostam_de_fritas
+FROM clientes
+WHERE clientes.nome = 'Paula' OR clientes.nome = 'Elise' OR clientes.nome = 'Marcelo';
 
 -- 4)
+SELECT SUM(CAST(produtos.preço AS decimal(10,2)))
+FROM produtos_pedidos
+JOIN produtos
+ON produtos_pedidos.produto_id  = produtos.id 
+WHERE produtos_pedidos.pedido_id  = 5;
 
 -- 5)
+SELECT produtos.nome,
+	   COUNT(produtos_pedidos.pedido_id)
+FROM produtos_pedidos
+JOIN produtos
+ON produtos_pedidos.produto_id = produtos.id
+GROUP BY produtos.nome;
